b3dfac015eb5ab11baad50e3a60a2b4328e51aca
usmandanish1710-png <usmandanish1710@gmail.com>
2026-01-13 09:55:22 +0500

    Add files via upload

---
+++ Natural Number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, N;
+    cout<<"Enter the N number of natural No.: ";
+    cin>>N;
+    for(n=1; n<=N; n++)
+    {
+        cout<<n<<endl;
+    }
+    return 0;
+}
